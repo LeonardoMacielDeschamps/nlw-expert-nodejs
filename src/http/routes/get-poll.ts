db20c1d50db66e8db0650d0385098432b684e508
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2024-02-12 01:03:18 -0300

    Aula 03

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 import { prisma } from '../../lib/prisma'
 import { FastifyInstance } from 'fastify'
+import { redis } from '../../lib/redis'
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -24,8 +25,39 @@ export async function getPoll(app: FastifyInstance) {
       },
     })
 
+    if (!poll) {
+      return reply.status(404).send({
+        message: 'Poll not found.',
+      })
+    }
+
+    const votesOnRedis = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+
+    const votes = votesOnRedis.reduce(
+      (previousValue, currentValue, currentIndex) => {
+        if (currentIndex % 2 === 0) {
+          const score = Number(votesOnRedis[currentIndex + 1])
+
+          Object.assign(previousValue, { [currentValue]: score })
+        }
+
+        return previousValue
+      },
+      {} as Record<string, number>,
+    )
+
     return reply.send({
-      poll,
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map(({ id, title }) => {
+          return {
+            id,
+            title,
+            score: id in votes ? votes[id] : 0,
+          }
+        }),
+      },
     })
   })
 }
--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
 import { prisma } from '../../lib/prisma'
 import { FastifyInstance } from 'fastify'
+import { redis } from '../../lib/redis'
+import { voting } from '../../utils/voting-pub-sub'
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -35,10 +37,18 @@ export async function voteOnPoll(app: FastifyInstance) {
           })
         }
 
-        await prisma.vote.delete({
-          where: {
-            id: userPreviousVoteOnPoll.id,
-          },
+        const [, votes] = await Promise.all([
+          prisma.vote.delete({
+            where: {
+              id: userPreviousVoteOnPoll.id,
+            },
+          }),
+          redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId),
+        ])
+
+        voting.publish(pollId, {
+          pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+          votes: Number(votes),
         })
       }
     }
@@ -54,12 +64,20 @@ export async function voteOnPoll(app: FastifyInstance) {
       })
     }
 
-    await prisma.vote.create({
-      data: {
-        sessionId,
-        pollId,
-        pollOptionId,
-      },
+    const [, votes] = await Promise.all([
+      prisma.vote.create({
+        data: {
+          sessionId,
+          pollId,
+          pollOptionId,
+        },
+      }),
+      redis.zincrby(pollId, 1, pollOptionId),
+    ])
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes),
     })
 
     return reply.status(201).send()
